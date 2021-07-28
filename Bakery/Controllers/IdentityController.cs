ac307b6da00f56c7bb5fede9eadcf04a3fa3b5e3
Maria Abrahamyan <maria.abrahamyan@dataart.com>
2021-07-28 16:45:27 +0400

    Controllers added with not implemented Endpoints

--- Bakery/Controllers/IdentityController.cs
+++ Bakery/Controllers/IdentityController.cs
@@ -2,42 +2,70 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bakery.Model;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bakery.Controllers
 {
 	[Route("api/[controller]")]
 	[ApiController]
-	public class IdentityController : ControllerBase 
+	public class IdentityController : ControllerBase
 	{
 		[HttpPost("RegisterUser")]
-		public async Task<IActionResult> RegisterUser()
+		public async Task<IActionResult> RegisterUser(RegisterViewModel registerVM)
 		{
 			return Ok();
 		}
 
-		[HttpGet("{id}")]
-		public async Task<IActionResult> LogIn(int id)
+		[HttpPost("SignIn")]
+		public async Task<IActionResult> SignIn(SignInViewModel signInVM)
 		{
 			return Ok();
 		}
 
-		// POST api/<IdentiryController>
-		[HttpPost]
-		public void Post([FromBody] string value)
+		[HttpGet("logOut/{id}")]
+		public async Task<IActionResult> LogOut(int id)
 		{
+			return Ok();
 		}
 
-		// PUT api/<IdentiryController>/5
-		[HttpPut("{id}")]
-		public void Put(int id, [FromBody] string value)
+		[HttpGet("GetUser/{id}")]
+		public async Task<IActionResult> GetUserById(int userId)
 		{
+			return Ok();
 		}
 
-		// DELETE api/<IdentiryController>/5
-		[HttpDelete("{id}")]
-		public void Delete(int id)
+		[HttpPut("ChangePassword")]
+		public async Task<IActionResult> ChangeUserPassword(UserViewModel userVM)
+        {
+			return Ok();
+        }
+
+		[HttpGet("ConfirmEmail/{id}")]
+		public async Task<IActionResult> RequestEmailConfirmation(string mail)
 		{
+			return Ok();
 		}
+
+		[HttpPost("EmailConfirmed/{confirmationId}")]
+		public async Task<IActionResult> EmailConfirmed(string confirmationId)
+		{
+			return Ok();
+		}
+
+		[HttpGet("ResetPassword/{username}")]
+		public async Task<IActionResult> ResetPassword(string username)
+		{
+			return Ok();
+		}
+
+
+
+
+
+
+
+
+
 	}
 }
